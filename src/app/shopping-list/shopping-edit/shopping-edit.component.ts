2f9481d8175934d9e63503d3d8b2e33e46349865
benoitVaisse <benoit.vaisse@gmail.com>
2021-05-10 17:43:13 +0200

    formulaire ajout ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component,  OnInit} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
@@ -9,14 +10,32 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild("inputAmount")inputAmout:ElementRef = new ElementRef("input");
+  ingredientForm:FormGroup = new FormGroup({});
   constructor(private shoppingListService:ShoppingListService) { }
 
   ngOnInit(): void {
+    this.ingredientForm = new FormGroup({
+      "name": new FormControl(null,[
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      "amount": new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/\d+/)
+      ])
+    })
   }
 
-  AddItem(name:string){
-    if(name !== "")
-      this.shoppingListService.addIngredient(new Ingredient(name, this.inputAmout.nativeElement.value));
+  onAddItem(){
+      if(this.ingredientForm.valid){
+        this.shoppingListService.addIngredient(new Ingredient( this.name?.value, this.amount?.value));
+      }
+  }
+
+  get name(){
+    return this.ingredientForm.get("name");
+  }
+  get amount(){
+    return this.ingredientForm.get("amount");
   }
 }
